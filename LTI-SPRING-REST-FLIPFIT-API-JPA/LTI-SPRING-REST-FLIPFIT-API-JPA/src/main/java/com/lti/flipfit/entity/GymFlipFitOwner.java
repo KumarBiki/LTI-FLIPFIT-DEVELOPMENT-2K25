8c7be7e6841846d26915fc81e71837638f74d6a0
Biki Kumar Sa <bikikumar.10845764@ltimindtree.com>
2025-11-20 13:20:20 +0530

    Initial commit for lti flip fit Spring Rest JPA app

---
+++ LTI-SPRING-REST-FLIPFIT-API-JPA/LTI-SPRING-REST-FLIPFIT-API-JPA/src/main/java/com/lti/flipfit/entity/GymFlipFitOwner.java
@@ -0,0 +1,16 @@
+package com.lti.flipfit.entity;
+
+public class GymFlipFitOwner extends GymFlipFitUser{
+	
+private String panNo;
+
+public String getPanNo() {
+	return panNo;
+}
+
+public void setPanNo(String panNo) {
+	this.panNo = panNo;
+}
+
+
+}
